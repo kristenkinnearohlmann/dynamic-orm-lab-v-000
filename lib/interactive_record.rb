6f122aea6e0a1cac61acc8f37e0c2464c08c57c2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-06 23:26:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,8 +39,8 @@ class InteractiveRecord
 
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
-      end
     end
+    values.join(", ")
   end
 
 end
